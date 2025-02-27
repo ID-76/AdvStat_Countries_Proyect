73e7b304c2f5f9867946b0ac088bfd5a047f74df
AlvarodelaHerran <alvarodela.herran@opendeusto.es>
2025-02-27 20:24:10 +0100

    a. Are the data observational or experimental?

--- MainProyecto.R
+++ MainProyecto.R
@@ -1,1 +1,9 @@
 #Proyecto AdvStat
+shrek <- read.csv("worlddata2023.csv", fileEncoding = "UTF-8")
+print(shrek)
+
+
+
+#a. Are the data observational or experimental?
+#The data is likely observational, 
+#reflecting economic and social indicators collected without experimental intervention.
